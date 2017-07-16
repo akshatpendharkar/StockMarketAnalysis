608a490e6ce8f5283f5576d58bf178e5625c5708
ap3ca <ap3ca@users.noreply.github.com>
2017-07-15 21:15:06 -0400

    changed S&P file

--- SandP500List.py
+++ SandP500List.py
@@ -24,8 +24,6 @@ def save_sp500_tickers():
         
     return tickers
 
-#save_sp500_tickers()
-
 
 def get_data_from_google(reload_sp500=False):
     
@@ -39,7 +37,7 @@ def get_data_from_google(reload_sp500=False):
         os.makedirs('stock_dfs')
 
     start = dt.datetime(2000, 1, 1)
-    end = dt.datetime(2016, 12, 31)
+    end = dt.datetime(2017, 7, 15)
     
     for ticker in tickers:
         if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
@@ -48,4 +46,29 @@ def get_data_from_google(reload_sp500=False):
         else:
             print('Already have {}'.format(ticker))
 
-get_data_from_google()
+
+def compile_data():
+    with open("sp500tickers.pickle","rb") as f:
+        tickers = pickle.load(f)
+
+    main_df = pd.DataFrame()
+    
+    for count,ticker in enumerate(tickers):
+        df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+        df.set_index('Date', inplace=True)
+
+        df.rename(columns={'Adj Close':ticker}, inplace=True)
+        df.drop(['Open','High','Low','Close','Volume'],1,inplace=True)
+
+        if main_df.empty:
+            main_df = df
+        else:
+            main_df = main_df.join(df, how='outer')
+
+        if count % 10 == 0:
+            print(count)
+    print(main_df.head())
+    main_df.to_csv('sp500_joined_closes.csv')
+
+
+compile_data()
